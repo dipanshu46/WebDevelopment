c7c4ca0c10c3c56da240a08eb14620c3308b32cf
Dipanshu <dipanshusingh588@gmail.com>
2023-05-08 20:06:00 +0530

    Modified topic2.js and added mongo_commands.txt, basic commands for handling database through mongo shell after connecting to a mongod process.

--- MONGODB/static/topic2.js
+++ MONGODB/static/topic2.js
@@ -1,6 +1,7 @@
 // Script injected by topic2.pug
 let content = [
     'MonogDB stores data as a document, so it is known as document oriented database',
+    'MongoDB does not forces you to follow a schema',
     'Data is stored as BSON', 
     'If you come from Relational DBMS background :-\n<ul>\n<li>Database = Database</li>\n<li>Table = Collections</li>\n<li>Rows = Documents</li>\n</ul>',
 ]
